c6ddb7ae62a00ec574ba7f183f632bd4e4da9550
JohnFreddy <jh0n.fr3ddym@gmail.com>
2014-05-28 21:32:59 -0500

    Tercer commit , agrego clase novedades

---
+++ src/Estudiantes/Novedades.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Estudiantes;
+
+/**
+ *
+ * @author adsi-admin
+ */
+public class Novedades extends Datos{
+    String cantidadauxilio;
+    String descripcion;
+
+    public String getCantidadauxilio() {
+        return cantidadauxilio;
+    }
+
+    public void setCantidadauxilio(String cantidadauxilio) {
+        this.cantidadauxilio = cantidadauxilio;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+    public void mostrardatos(){
+
+    }
+    
+}
